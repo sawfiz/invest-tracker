4fcbb7b916c81b3e59ddf1a252614866dac0aa05
Victor Zhang <112825751+sawfiz@users.noreply.github.com>
2024-04-02 10:35:22 +0800

    Set up Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <>
-    <h1>Investment Tracker</h1>
+      <h1>Investment Tracker</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum expedita
+      deserunt illo rem architecto quos enim nobis animi quia ipsum. Veritatis
+      rem sit quidem aliquam possimus omnis, dicta vero eligendi adipisci,
+      dolorum repellendus laborum. Alias voluptas nisi sapiente suscipit maiores
+      adipisci accusamus placeat, reiciendis, iste iusto aperiam sequi, quasi
+      fugiat ratione a tenetur harum? Asperiores quos suscipit numquam, harum
+      hic adipisci possimus reprehenderit aliquid maxime veritatis quasi
+      quisquam sint consequuntur dicta dolores porro nobis architecto
+      necessitatibus at error tempora! Accusamus quas dolorum est similique
+      animi aspernatur eos officia id, rem harum obcaecati ipsa totam soluta
+      nostrum nihil cumque doloribus non!
     </>
   );
 }
