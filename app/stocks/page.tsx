cb8496c1008f0e911133e6b8061b3346b53c06d7
Victor Zhang <112825751+sawfiz@users.noreply.github.com>
2024-04-02 12:46:59 +0800

    Set up Radix UI themes

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 const StocksPage = () => {
   return (
-    <div>Stocks Page</div>
+    <div>
+      <h1>Stocks Page</h1>
+      <Button>New Trade</Button>
+    </div>
   )
 }
 
