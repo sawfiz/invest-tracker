9df27fd4046ed0f20180bab48d8360e3ae484ea9
Victor Zhang <112825751+sawfiz@users.noreply.github.com>
2024-04-08 15:53:59 +0800

    Add fetching yahoo finance data

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AddStockTrade from "./AddStockTrade";
 import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
+import yahooFinance from "yahoo-finance2";
 
 const StocksPage = async () => {
   const stockTrades = await prisma.stockTrade.findMany();
@@ -15,32 +16,50 @@ const StocksPage = async () => {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Trader</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Broker</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Ticker</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Shares</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Price</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Fees</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Amount</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Notes</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className=" text-right">Shares</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className=" text-right">Cost</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className=" text-right">Price</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className=" text-right">P/L</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className=" text-right">P/L %</Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {stockTrades.map((trade) => (
-            <Table.Row key={trade.id}>
-              <Table.Cell>{trade.date.toDateString()}</Table.Cell>
-              <Table.Cell>{trade.trader}</Table.Cell>
-              <Table.Cell>{trade.broker}</Table.Cell>
-              <Table.Cell>{trade.ticker}</Table.Cell>
-              <Table.Cell>{trade.action}</Table.Cell>
-              <Table.Cell>{trade.shares}</Table.Cell>
-              <Table.Cell>{trade.price}</Table.Cell>
-              <Table.Cell>{trade.fees}</Table.Cell>
-              <Table.Cell>{trade.amount}</Table.Cell>
-              <Table.Cell>{trade.notes}</Table.Cell>
-            </Table.Row>
-          ))}
+          {stockTrades.map(async (trade) => {
+            const quote = await yahooFinance.quote(trade.ticker);
+            console.log("🚀 ~ {stockTrades.map ~ currentPrice:", quote);
+            return (
+              <Table.Row key={trade.id}>
+                <Table.Cell>
+                  {trade.date.toDateString().substring(0, 3) +
+                    " " +
+                    trade.date.toISOString().substring(0, 10)}
+                </Table.Cell>
+                <Table.Cell>{trade.ticker}</Table.Cell>
+                <Table.Cell className=" text-right">{trade.shares }</Table.Cell>
+                <Table.Cell className=" text-right">{trade.price}</Table.Cell>
+                <Table.Cell className=" text-right">{quote.regularMarketPrice}</Table.Cell>
+                <Table.Cell className=" text-right">
+                  {(
+                    (quote?.regularMarketPrice - trade.price) *
+                    trade.shares
+                  ).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </Table.Cell>
+                <Table.Cell className=" text-right">
+                  {(
+                    (quote?.regularMarketPrice / trade.price - 1) *
+                    100
+                  ).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  }) + "%"}
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
         </Table.Body>
       </Table.Root>
     </div>
